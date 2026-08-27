5bc312784f3d4962f97198bdea00d898ad5f2aca
ritheeckdharan <87609543+ritheeckdharan@users.noreply.github.com>
2026-08-28 00:29:28 +0530

    Add files via upload

---
+++ jstask.js
@@ -0,0 +1,104 @@
+//var
+
+var a = 10;
+console.log(a);
+
+//16th question the output is 10
+
+var a = 10;
+a = 20;
+console.log(a);
+//17th question the output is 20
+
+var a = 10;
+var a = 30;
+console.log(a);
+//18th question the output is 30
+
+var name="John";
+console.log(name);
+
+//19th question the output is John
+
+var price=500;
+price=1000;
+console.log(price)
+//20th and 21th question the output is 1000
+
+var x = 50;
+x = 100;
+console.log(x);
+//22th question the output is 100
+
+//let
+
+let age=25;
+console.log(age);
+//26th question the output is 25
+
+let age1= 20;
+age1 = 30;
+console.log(age1);
+//27th  question the output is 30'
+
+let city="Chennai";
+city="Salem";
+console.log(city);
+//31th and 32th question the output is Salem
+
+
+let x1 = 10;
+x1 = 50;
+console.log(x1);
+// 33th question the output is 50
+
+let salary=25000;
+salary=30000;
+console.log(salary);
+//34th and 35th question the output is 30000
+
+
+//const
+
+const pi=3.14;
+console.log(pi);
+//36 question the output is 3.14
+
+const country="India";
+console.log(country);
+//41 question the output is India
+
+const x2 = 100;
+console.log(x2);
+//42 question the output is 100
+
+
+//Printing Console 
+var b="Hello World";
+console.log(b);
+//46 question the output is Hello world
+
+var c=500;
+console.log(c);
+//47 question the output is 500
+
+
+///Practical Questions
+
+let user_name="admin";
+let age2=23;
+let marks=80;
+console.log(user_name,age,marks);
+
+
+let username=prompt("Please enter your name");
+alert("Your name is " + username);
+
+let agep=prompt("Please enter yr age");
+console.log("your age is" + agep);
+
+let  d="Your favorite color"
+confirm(d);
+
+let userd=prompt("enter yr name");
+document.write(userd);
